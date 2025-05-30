63a999b3e6b3c258332663399717a9dccc46c283
wswangdaiping <167610925+wswangdaiping@users.noreply.github.com>
2025-05-30 23:39:40 +0800

    translations

---
+++ translations/main.zh_CN.ts
@@ -0,0 +1,265 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>BucketheadZombie</name>
+    <message>
+        <location filename="../src/Zombie.cpp" line="424"/>
+        <source>Buckethead Zombie</source>
+        <translation>铁桶僵尸</translation>
+    </message>
+</context>
+<context>
+    <name>ConeheadZombie</name>
+    <message>
+        <location filename="../src/Zombie.cpp" line="379"/>
+        <source>Conehead Zombie</source>
+        <translation>路障僵尸</translation>
+    </message>
+</context>
+<context>
+    <name>FlagZombie</name>
+    <message>
+        <location filename="../src/Zombie.cpp" line="67"/>
+        <source>Flag Zombie</source>
+        <translation>旗帜僵尸</translation>
+    </message>
+</context>
+<context>
+    <name>GameLevelData_1</name>
+    <message>
+        <location filename="../src/GameLevelData.cpp" line="68"/>
+        <source>Level 1-1</source>
+        <translation>关卡 1-1</translation>
+    </message>
+</context>
+<context>
+    <name>GameScene</name>
+    <message>
+        <location filename="../src/GameScene.cpp" line="33"/>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="34"/>
+        <source>Go</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <source>Menu</source>
+        <translation type="vanished">菜  单</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="122"/>
+        <source>Choose your cards</source>
+        <translation>选择你的卡片</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="160"/>
+        <source>Nocturnal - sleeps during day</source>
+        <translation>夜行性的--在白天会睡觉</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="321"/>
+        <source>Plants vs. Zombies</source>
+        <translation>植物大战僵尸</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="341"/>
+        <source>%1&apos; house</source>
+        <translation>%1的房间</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="158"/>
+        <location filename="../src/GameScene.cpp" line="606"/>
+        <source>Cool down: %1s</source>
+        <translation>冷却时间：%1秒</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="94"/>
+        <source>Back</source>
+        <translation>返  回</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="609"/>
+        <source>Rechanging...</source>
+        <translation>正在重新装填中...</translation>
+    </message>
+    <message>
+        <location filename="../src/GameScene.cpp" line="611"/>
+        <source>Not enough sun!</source>
+        <translation>阳光不足！</translation>
+    </message>
+</context>
+<context>
+    <name>LawnCleaner</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="266"/>
+        <source>Lawn Cleaner</source>
+        <translation>草地剪草机</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="273"/>
+        <source>Normal lawn cleaner</source>
+        <translation>最普通的草地剪草机</translation>
+    </message>
+</context>
+<context>
+    <name>MainView</name>
+    <message>
+        <location filename="../src/MainView.cpp" line="36"/>
+        <source>Guest</source>
+        <translation>游客</translation>
+    </message>
+</context>
+<context>
+    <name>Peashooter</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="138"/>
+        <source>Peashooter</source>
+        <translation>豌豆射手</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="144"/>
+        <source>Shoots peas at zombies</source>
+        <translation>向敌人射出豌豆</translation>
+    </message>
+</context>
+<context>
+    <name>Peashooter</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="163"/>
+        <source>Repeater</source>
+        <translation>双发射手</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="169"/>
+        <source>Shoots two peas at a time</source>
+        <translation>向敌人连续射出两个豌豆</translation>
+    </message>
+</context>
+<context>
+    <name>Peashooter</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="208"/>
+        <source>Threepeater</source>
+        <translation>三重射手</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="214"/>
+        <source>Simultaneously shoots peas to three lanes</source>
+        <translation>三线攻击</translation>
+    </message>
+</context>
+<context>
+    <name>PoleVaultingZombie</name>
+    <message>
+        <location filename="../src/Zombie.cpp" line="440"/>
+        <source>Pole Vaulting Zombie</source>
+        <translation>撑杆僵尸</translation>
+    </message>
+</context>
+<context>
+    <name>PoolCleaner</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="279"/>
+        <location filename="../src/Plant.cpp" line="282"/>
+        <source>Pool Cleaner</source>
+        <translation>池塘清扫车</translation>
+    </message>
+</context>
+<context>
+    <name>PumpkinHead</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="430"/>
+        <source>Pumpkin Head</source>
+        <translation>南瓜头</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="438"/>
+        <source>Protects the plant inside it</source>
+        <translation>能保护种在里面的植物</translation>
+    </message>
+</context>
+<context>
+    <name>SelectorScene</name>
+    <message>
+        <location filename="../src/SelectorScene.cpp" line="139"/>
+        <source>Plants vs. Zombies</source>
+        <translation>植物大战僵尸</translation>
+    </message>
+</context>
+<context>
+    <name>SnowPea</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="162"/>
+        <source>Snow Pea</source>
+        <translation>寒冰射手</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="169"/>
+        <source>Slows down zombies with cold precision</source>
+        <translation>寒冰射手可造成伤害, 同时又有减速效果</translation>
+    </message>
+</context>
+<context>
+    <name>SunFlower</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="187"/>
+        <source>Sunflower</source>
+        <translation>向日葵</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="193"/>
+        <source>Makes extra Sun for placing plants</source>
+        <translation>提供你额外的阳光</translation>
+    </message>
+</context>
+<context>
+    <name>TallNut</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="543"/>
+        <source>Tall Nut</source>
+        <translation>高坚果</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="547"/>
+        <source>Heavy-duty wall plants that can&apos;t be vaulted or jumped over</source>
+        <translation>不会被跳过的坚实壁垒</translation>
+    </message>
+</context>
+<context>
+    <name>Torchwood</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="524"/>
+        <source>Torchwood</source>
+        <translation>火炬树桩</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="527"/>
+        <source>Turns peas that pass through them into fireballs</source>
+        <translation>通过火炬树桩的豌豆将变为火球</translation>
+    </message>
+</context>
+<context>
+    <name>WallNut</name>
+    <message>
+        <location filename="../src/Plant.cpp" line="239"/>
+        <source>Wall-nut</source>
+        <translation>坚果墙</translation>
+    </message>
+    <message>
+        <location filename="../src/Plant.cpp" line="247"/>
+        <source>Stops zombies with its chewy shell</source>
+        <translation>阻碍僵尸前进, 并保护你其他的植物</translation>
+    </message>
+</context>
+<context>
+    <name>Zombie1</name>
+    <message>
+        <location filename="../src/Zombie.cpp" line="36"/>
+        <source>Zombie</source>
+        <translation>领带僵尸</translation>
+    </message>
+</context>
+</TS>
